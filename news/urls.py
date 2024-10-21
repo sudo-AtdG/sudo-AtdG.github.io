71c3e05b0fae2771c6e6ac902a1c190ec92462be
astat <hlebbo4ka@ukr.net>
2024-10-21 14:17:45 +0300

    Delete news directory

--- news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.news_index, name='news_index'),
-    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail')
-]
